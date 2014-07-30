1880a3f804668f401e94c9c36c03d09c0cf5c36f
n3utrinoCH <pascal.zirn@bluewin.ch>
2014-07-30 17:22:20 +0200

    Update lazyloader.js

--- lazyloader.js
+++ lazyloader.js
@@ -4,40 +4,43 @@
 
 window.lazyloader = (function($) {
 
-	var $w = $(window), selector, injectDist, replaceORappend;
+    var $w = $(window),
+        selector, injectDist, replaceORappend;
 
-	var proximityLoad = function($el) {
-		var elY 	= $el.offset().top,
-			content = $el.attr(selector);
+    var proximityLoad = function($el) {
+        var elY = $el.offset().top,
+            content = $el.attr(selector);
 
-		var proximityCheck = function() {
-			$el.dYb = Math.abs( elY - (window.pageYOffset + window.innerHeight) );
-			$el.dYt = Math.abs( elY - window.pageYOffset );
+        var proximityCheck = function() {
+            $el.dYb = Math.abs(elY - (window.pageYOffset + window.innerHeight));
+            $el.dYt = Math.abs(elY - window.pageYOffset);
 
-			if ($el.dYb < injectDist || $el.dYt < injectDist) {
-				$w.unbind('scroll.lazyproxyload', proximityCheck);
-				replaceORappend === 'append' ? $el.append(content) : $el.html(content);
-			}
-		};
+            if ($el.dYb < injectDist || $el.dYt < injectDist) {
+                $w.unbind('scroll.lazyproxyload', proximityCheck);
+                replaceORappend === 'append' ? $el.append(content) : $el.html(content);
+            }
+        };
 
-		$w.bind('scroll.lazyproxyload', proximityCheck);
+        $w.bind('scroll.lazyproxyload', proximityCheck);
 
-		proximityCheck();
-	};
+        proximityCheck();
+    };
 
-	var init = function(distance, replace, jqSelector) {
-		injectDist 		= distance || 400;
-		replaceORappend = replace || 'replace';
-		selector 		= jqSelector || 'data-lazyloadcontent';
+    var init = function(distance, replace, jqSelector) {
+        injectDist = distance || 400;
+        selector = jqSelector || 'data-lazyloadcontent';
+        replaceORappend = replace || 'replace';
 
-		$.each( $('['+selector+']'), function() {
-			proximityLoad($(this));
-		});
-	};
+        $.each($('[' + selector + ']'), function() {
+            proximityLoad($(this));
+        });
+    };
 
-	return { init: init	}
+    return {
+        init: init
+    }
 
 })(window.jQuery);
 
 
-$( lazyloader.init(300, 'append', 'data-lazyloadcontent') );
+$(lazyloader.init(300, 'append', 'data-lazyloadcontent'));
