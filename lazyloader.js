239af8fccad98c3dad37463a29354b1f40a63e9f
n3utrinoCH <pascal.zirn@bluewin.ch>
2014-07-30 17:20:12 +0200

    Create lazyloader.js

---
+++ lazyloader.js
@@ -0,0 +1,43 @@
+/*
+	by Pascal "Mason" Zirn | pascal.zirn@20minuten.ch
+*/
+
+window.lazyloader = (function($) {
+
+	var $w = $(window), selector, injectDist, replaceORappend;
+
+	var proximityLoad = function($el) {
+		var elY 	= $el.offset().top,
+			content = $el.attr(selector);
+
+		var proximityCheck = function() {
+			$el.dYb = Math.abs( elY - (window.pageYOffset + window.innerHeight) );
+			$el.dYt = Math.abs( elY - window.pageYOffset );
+
+			if ($el.dYb < injectDist || $el.dYt < injectDist) {
+				$w.unbind('scroll.lazyproxyload', proximityCheck);
+				replaceORappend === 'append' ? $el.append(content) : $el.html(content);
+			}
+		};
+
+		$w.bind('scroll.lazyproxyload', proximityCheck);
+
+		proximityCheck();
+	};
+
+	var init = function(distance, replace, jqSelector) {
+		injectDist 		= distance || 400;
+		replaceORappend = replace || 'replace';
+		selector 		= jqSelector || 'data-lazyloadcontent';
+
+		$.each( $('['+selector+']'), function() {
+			proximityLoad($(this));
+		});
+	};
+
+	return { init: init	}
+
+})(window.jQuery);
+
+
+$( lazyloader.init(300, 'append', 'data-lazyloadcontent') );
